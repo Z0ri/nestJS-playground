9764a39368dfad8a41651f86b6ee9c5e6b5ca38e
Z0ri <l.rossi@gsa.eco>
2025-05-07 15:44:50 +0200

    owner relation added

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -1,6 +1,7 @@
 import { ProductInterface } from 'src/interfaces/product.interface';
 import { CommonEntity } from './common.entity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { UserEntity } from './user.entity';
 
 @Entity('products')
 export class ProductEntity extends CommonEntity implements ProductInterface {
@@ -21,14 +22,21 @@ export class ProductEntity extends CommonEntity implements ProductInterface {
     }
   }
 
+  @ManyToOne(() => UserEntity, (user) => user.products, { nullable: true })
+  owner: UserEntity | null;
+
   @Column({ type: 'varchar', nullable: false })
   title: string;
+
   @Column({ nullable: false })
   price: number;
+
   @Column({ type: 'varchar' })
   description: string;
+
   @Column({ type: 'varchar', nullable: false })
   category: string;
+
   @Column({ type: 'varchar' })
   image: string;
 }
