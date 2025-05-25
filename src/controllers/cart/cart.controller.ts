8f63a7a50cd5fd0fa787d653c42b840ac682666f
Z0ri <loriross2005@gmail.com>
2025-05-25 18:10:29 +0200

    get user cart items, get cart items, add item to user cart

--- src/controllers/cart/cart.controller.ts
+++ src/controllers/cart/cart.controller.ts
@@ -1,20 +1,39 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post, Query, Req, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/decorators/roles/roles.decorator';
+import { RoleEnum } from 'src/enum/Roles';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
+import { RolesGuard } from 'src/guards/roles/roles.guard';
+import { RequestWithUser } from 'src/interfaces/requestWithUser.interface';
 import { CartService } from 'src/services/cart/cart.service';
 
 @Controller('cart')
+@UseGuards(AuthGuard, RolesGuard)
 export class CartController {
 
     constructor(
         private cartService: CartService
     ){}
 
+    @Get('item/user')
+    @Roles(RoleEnum.Admin, RoleEnum.Editor, RoleEnum.Viewer)
+    async getUserCartItems(
+        @Req() request: Request & { user: RequestWithUser }, 
+    ){
+        return await this.cartService.getUserCartItems(request.user.key);
+    }
+
+    @Get('item')
+    @Roles(RoleEnum.Admin, RoleEnum.Editor, RoleEnum.Viewer)
+    async getCartItem(@Query('id') id: number){
+        return await this.cartService.getCartItems(id);
+    }
+
     @Post('item')
-    async addItem(
-    @Body() body: {
-        cartId: number,
-        productId: number
-    }){
-        const { productId, cartId } = body;
-        return await this.cartService.addItem(productId, cartId);
+    @Roles(RoleEnum.Admin, RoleEnum.Editor, RoleEnum.Viewer)
+    async addUserItem(
+        @Query('key') productKey: string,
+        @Req() request: Request & {user: RequestWithUser}
+    ){
+        return await this.cartService.addItemToUserCart(productKey, request.user.key);
     }
 }
--- src/services/cart/cart.service.ts
+++ src/services/cart/cart.service.ts
@@ -4,7 +4,6 @@ import { CartEntity } from 'src/entities/cart.entity';
 import { CartItemsEntity } from 'src/entities/cartItems.entity';
 import { ProductEntity } from 'src/entities/product.entity';
 import { UserEntity } from 'src/entities/user.entity';
-import { ProductInterface } from 'src/interfaces/product.interface';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -65,25 +64,127 @@ export class CartService {
     }
 
     /**
-     * Aggiunge un prodotto ad un carrello, se già presente, aumenta la quantità di quest'ultimo
-     * @param productId id del prodotto da aggiungere
-     * @param cartId id del carrello su cui aggiungere il prodotto
-     * @returns item creato
+     * Permette di ottenere tutti i prodotti all'interno di un carrello
+     * @param cartId id del carrello
+     * @returns array di cart entity trovate
      */
-    async addItem(productId: number, cartId: number) {
-        if (!productId) {
+    async getCartItems(cartId: number){
+        if(!cartId){
+            throw new HttpException({
+                message: "Nessun id di carrello fornito"
+            }, HttpStatus.BAD_REQUEST)
+        }
+
+        try {
+            const cart: CartEntity = await this.cartReadonlyRepository.findOne({
+                where: {
+                    id: cartId
+                }
+            });
+
+            if(!cart){
+                throw new HttpException({
+                message: "L'id fornito non appartiene a nessun carrello esistente"
+                }, HttpStatus.NOT_FOUND);
+            }
+
+            return this.cartItemsReadonlyRepository.find(
+                { 
+                    where: {
+                        cartId: cartId
+                    }
+                }
+            )
+        } catch (error) {
+            
+        }
+    }
+
+    /**
+     * Aggiunge un prodotto al carrello di un utente
+     * @param productKey key del prodotto da aggiungere
+     * @param userKey key dell'utente che vuole aggiungere il prodotto
+     * @returns 
+     */
+    async addItemToUserCart(productKey: string, userKey: string): Promise<CartItemsEntity>{
+        if (!productKey) {
             throw new HttpException(
                 {
-                    message: "Nessun id di un prodotto fornito"
+                    message: "Nessuna key di un prodotto fornita"
+                }, 
+                HttpStatus.BAD_REQUEST
+            );
+        }
+        if (!userKey) {
+            throw new HttpException(
+                {
+                    message: "Nessuna key di un utente fornita"
                 }, 
                 HttpStatus.BAD_REQUEST
             );
         }
 
-        if (!cartId) {
+        try {
+            const user = await this.userReadonlyRepository.findOne({
+                where: { key: userKey }
+            });
+
+            if (!user) {
+                throw new HttpException(
+                    {
+                        message: "Utente non trovato"
+                    }, 
+                    HttpStatus.NOT_FOUND
+                );
+            }
+
+            const cart = await this.cartReadonlyRepository.findOne({
+                where: {
+                    userId: user.id
+                },
+            });
+
+            if(!cart){
+                throw new HttpException(
+                    {
+                        message: "Nessun carrello associato all'utente trovato"
+                    }, 
+                    HttpStatus.NOT_FOUND
+                );
+            }
+
+            return await this.addItemToCart(productKey, cart.key);
+        } catch (error) {
+            if(error instanceof HttpException) throw error;
+
+            throw new HttpException({
+                message: "Errore nella ricerca del carrello dell'utente"
+            },
+            HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    /**
+     * Aggiunge un prodotto ad un carrello, se già presente, aumenta la quantità di quest'ultimo
+     * @param productKey key del prodotto da aggiungere
+     * @param cartKey key del carrello su cui aggiungere il prodotto
+     * @returns item creato
+     */
+    async addItemToCart(productKey: string, cartKey: string) {
+        if (!productKey) {
             throw new HttpException(
                 {
-                    message: "Nessun id del carrello fornito"
+                    message: "Nessuna key di un prodotto fornita"
+                }, 
+                HttpStatus.BAD_REQUEST
+            );
+        }
+
+        if (!cartKey) {
+            throw new HttpException(
+                {
+                    message: "Nessuna key del carrello fornita"
                 }, 
                 HttpStatus.BAD_REQUEST
             );
@@ -93,10 +194,10 @@ export class CartService {
             //ricerca parallela
             const [product, cart] = await Promise.all([
                 this.productReadonlyRepository.findOne({
-                    where: { id: productId }
+                    where: { key: productKey }
                 }),
                 this.cartReadonlyRepository.findOne({
-                    where: { id: cartId }
+                    where: { key: cartKey }
                 })
             ]);
 
@@ -112,7 +213,7 @@ export class CartService {
             if (!cart) {
                 throw new HttpException(
                     {
-                        message: "Carrello con l'id specificato non trovato"
+                        message: "Carrello con la key specificata non trovato"
                     }, 
                     HttpStatus.NOT_FOUND
                 );
@@ -130,12 +231,20 @@ export class CartService {
             //controllo se l'item è già presente nel carrello
             const existingCartItem: CartItemsEntity = await this.cartItemsReadonlyRepository.findOne({
                 where: {
-                    cartId: cartId,
-                    productId: productId
+                    cartId: cart.id,
+                    productId: product.id
                 }
             });
 
             if (existingCartItem) {
+                //controllo se aggiungendo +1 alla quantità del prodotto nel carrello supera la quantità del prodotto online
+                if(existingCartItem.quantity + 1 > product.quantity){
+                    throw new HttpException({
+                        message: "Quantità disponibile insufficente"
+                    },
+                    HttpStatus.CONFLICT);
+                }
+
                 existingCartItem.quantity = (existingCartItem.quantity || 1) + 1; //incremento della quantità se l'item già esiste
                 await this.cartItemsWriteonlyRepository.save(existingCartItem);
                 return existingCartItem;
@@ -143,8 +252,8 @@ export class CartService {
 
             //creazione di un nuovo cart item
             const cartItem = this.cartItemsWriteonlyRepository.create({
-                cartId: cartId,
-                productId: productId,
+                cartId: cart.id,
+                productId: product.id,
                 quantity: 1 
             });
 
@@ -160,6 +269,41 @@ export class CartService {
                 }, 
                 HttpStatus.INTERNAL_SERVER_ERROR
             );
+        }
+    }
+
+    /**
+     * Permette di ottenere i prodotti all'interno del carrello associato ad un utente
+     * @param userKey key dell'utente con il carrello associato da cui prendere i prodotti
+     * @returns array di prodotti all'interno del carrello
+     */
+    async getUserCartItems(userKey: string): Promise<CartItemsEntity[]>{
+        if (!userKey) {
+            throw new HttpException(
+                {
+                    message: "Nessuna use key passata"
+                }, 
+                HttpStatus.BAD_REQUEST
+            );
+        }
+
+        try {
+            const user: UserEntity = await this.userReadonlyRepository.findOne({
+                where: {
+                    key: userKey
+                },
+                relations: {
+                    cart: true
+                }
+            });
+
+            return await this.getCartItems(user.cart.id);
+        } catch (error) {
+            if(error instanceof HttpException) throw error;
+            throw new HttpException({
+                message: "Errore durante la ricerca dei prodotti all'interno del carrello dell'utente",
+                error: error.message
+            }, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
